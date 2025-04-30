ab1c5fece9a3b46080a244b3e1d571ff3ad5eaf1
Aayush Chaudhary <aayushcyy@gmail.com>
2025-04-30 18:49:31 +0530

    footer complete

--- app/Component/Footer.jsx
+++ app/Component/Footer.jsx
@@ -1,5 +1,99 @@
+import Link from "next/link";
 import React from "react";
+import Logo from "../../public/logo.png";
+import Image from "next/image";
 
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className="w-full h-[50vh] flex flex-col justify-between border-t-[2px] border-gray-300 pt-10">
+      <div className="flex justify-between px-60">
+        <div>
+          <Image src={Logo} width={600} alt="" className="w-32" />
+        </div>
+        <div className="flex flex-col gap-4">
+          <div className="uppercase text-gray-700 font-medium">Company</div>
+          <div className="flex flex-col text-sm text-gray-500 font-medium gap-1">
+            <Link href={"/"} className="hover:text-[#005854]">
+              About us
+            </Link>
+            <Link href={"/"} className="hover:text-[#005854]">
+              Careers
+            </Link>
+            <Link href={"/"} className="hover:text-[#005854]">
+              Blog
+            </Link>
+            <Link href={"/"} className="hover:text-[#005854]">
+              Terms
+            </Link>
+            <Link href={"/"} className="hover:text-[#005854]">
+              Contact us
+            </Link>
+          </div>
+        </div>
+        <div className="flex flex-col gap-4">
+          <div className="uppercase text-gray-700 font-medium">Resources</div>
+          <div className="flex flex-col text-sm text-gray-500 font-medium gap-1">
+            <Link href={"/"} className="hover:text-[#005854]">
+              Help
+            </Link>
+            <Link href={"/"} className="hover:text-[#005854]">
+              Referral program
+            </Link>
+            <Link href={"/"} className="hover:text-[#005854]">
+              Advanced
+            </Link>
+          </div>
+        </div>
+        <div className="flex flex-col gap-4">
+          <div className="uppercase text-gray-700 font-medium">Industry</div>
+          <div className="flex flex-col text-sm text-gray-500 font-medium gap-1">
+            <Link href={"/"} className="hover:text-[#005854]">
+              Venture Capital
+            </Link>
+            <Link href={"/"} className="hover:text-[#005854]">
+              Real Estate
+            </Link>
+            <Link href={"/"} className="hover:text-[#005854]">
+              Startups
+            </Link>
+            <Link href={"/"} className="hover:text-[#005854]">
+              Small Businesses
+            </Link>
+            <Link href={"/"} className="hover:text-[#005854]">
+              Travel Agencies
+            </Link>
+            <Link href={"/"} className="hover:text-[#005854]">
+              Influencer Agencies
+            </Link>
+          </div>
+        </div>
+        <div className="flex flex-col gap-4">
+          <div className="uppercase text-gray-700 font-medium">Feature</div>
+          <div className="flex flex-col text-sm text-gray-500 font-medium gap-1">
+            <Link href={"/"} className="hover:text-[#005854]">
+              CRM
+            </Link>
+            <Link href={"/"} className="hover:text-[#005854]">
+              Email tracking
+            </Link>
+            <Link href={"/"} className="hover:text-[#005854]">
+              Mail merge
+            </Link>
+            <Link href={"/"} className="hover:text-[#005854]">
+              Send later
+            </Link>
+            <Link href={"/"} className="hover:text-[#005854]">
+              Snippets
+            </Link>
+            <Link href={"/"} className="hover:text-[#005854]">
+              Email sharing
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className="w-full text-center text-xs flex flex-col gap-1.5 py-1 bg-[#005753] text-white">
+        <p>© 2016-2025 UniByte Pvt Ltd. - Privacy | Terms of Service</p>
+      </div>
+    </div>
+  );
 }
--- app/page.jsx
+++ app/page.jsx
@@ -5,6 +5,7 @@ import HeroSection2 from "./Component/HeroSection2";
 import Section3 from "./Component/Section3";
 import FeedbackPage from "./Component/FeedbackPage";
 import Section5 from "./Component/Section5";
+import Footer from "./Component/Footer";
 
 export default function Home() {
   return (
@@ -16,6 +17,7 @@ export default function Home() {
       <Section3 />
       <FeedbackPage />
       <Section5 />
+      <Footer />
     </div>
   );
 }
