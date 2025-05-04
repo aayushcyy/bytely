296394efb9a53e2e9b10f14cf940e43276749ffd
Aayush Chaudhary <aayushcyy@gmail.com>
2025-05-04 16:16:52 +0530

    added two openings

--- app/careers/page.jsx
+++ app/careers/page.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import Navbar from "../Component/Navbar";
 import { ButtonDemo } from "../Component/Button";
+import { CheckIcon } from "@heroicons/react/24/outline";
 import {
-  ComputerDesktopIcon,
-  TagIcon,
-  ChatBubbleLeftRightIcon,
-  CheckIcon,
-} from "@heroicons/react/24/outline";
-import Link from "next/link";
-import { AccordionDemo } from "../Component/Accordion";
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 
 export default function page() {
   return (
@@ -129,12 +128,176 @@ export default function page() {
           </div>
           <span className="h-[1px] bg-gray-300 w-full my-10" />
           {/* Engineering */}
-          <div>
+          <div className="flex flex-col gap-3">
             <p className="text-lg font-semibold">
               Engineering{" "}
               <span className="text-[#0B504B] text-base">( 1 )</span>
             </p>
-            <AccordionDemo outline="Staff UI Engineer" />
+            {/* Engineerning post */}
+            <div>
+              <Accordion type="single" collapsible className="w-[45%]">
+                <AccordionItem
+                  value="item-1"
+                  className="rounded-sm bg-white shadow border-[1px] border-gray-200 px-4"
+                >
+                  <AccordionTrigger className="hover:no-underline cursor-pointer">
+                    <div className="flex w-full justify-between">
+                      <div>
+                        <p className="font-medium">Staff UI Engineer</p>
+                        <span className="text-gray-500 text-xs font-normal">
+                          Ship user facing features
+                        </span>
+                      </div>
+                      <p className="rounded-lg bg-[#006d662a] text-[#0e5954] text-[10px] uppercase font-normal px-1.5 h-[1.2rem] flex items-center">
+                        Remote
+                      </p>
+                    </div>
+                  </AccordionTrigger>
+                  <AccordionContent>
+                    <div className="border-t-[1px] border-gray-200 pt-4 gap-5 flex flex-col">
+                      <div className="flex flex-col gap-1">
+                        <p className="font-medium text-base pb-1">
+                          Your skills
+                        </p>
+                        <p className="font-medium">
+                          • Experience:{" "}
+                          <span className="font-normal">
+                            5+ years with React & TypeScript.
+                          </span>
+                        </p>
+                        <p className="font-medium">
+                          • Strong Communicator:{" "}
+                          <span className="font-normal">
+                            Excellent async/written skills (docs, Looms,
+                            summaries) for remote-first teamwork.
+                          </span>
+                        </p>
+                        <p className="font-medium">
+                          • Collaborative & Pragmatic:{" "}
+                          <span className="font-normal">
+                            You review code, mentor peers, and balance new
+                            features with technical debt.
+                          </span>
+                        </p>
+                      </div>
+                      <div className="flex flex-col gap-1">
+                        <p className="font-medium text-base pb-1">
+                          What You'll Work On
+                        </p>
+                        <p>
+                          • High-performance real-time spreadsheets. <br />
+                          • Deep Gmail UI integrations <br />
+                          • Fast, local-first sync architecture <br />
+                          • AI-powered CRM workflows <br />
+                          • Reusable component systems <br />
+                          • Dev/build/test tooling for browser extensions <br />
+                          • Custom data visualizations & dashboards <br />•
+                          Funnel optimization for user conversion
+                        </p>
+                      </div>
+                      <ButtonDemo
+                        text="Apply"
+                        className="bg-[#08514D] text-white cursor-pointer"
+                      />
+                    </div>
+                  </AccordionContent>
+                </AccordionItem>
+              </Accordion>
+            </div>
+          </div>
+          {/* Sales */}
+          <div className="flex flex-col gap-3">
+            <p className="text-lg font-semibold">
+              Success <span className="text-[#0B504B] text-base">( 1 )</span>
+            </p>
+            {/* Engineerning post */}
+            <div>
+              <Accordion type="single" collapsible className="w-[45%]">
+                <AccordionItem
+                  value="item-1"
+                  className="rounded-sm bg-white shadow border-[1px] border-gray-200 px-4"
+                >
+                  <AccordionTrigger className="hover:no-underline cursor-pointer">
+                    <div className="flex w-full justify-between">
+                      <div>
+                        <p className="font-medium">Customer Success Manager</p>
+                        <span className="text-gray-500 text-xs font-normal">
+                          Tackle hard problems and delight our users
+                        </span>
+                      </div>
+                      <p className="rounded-lg bg-[#006d662a] text-[#0e5954] text-[10px] uppercase font-normal px-1.5 h-[1.2rem] flex items-center">
+                        Remote
+                      </p>
+                    </div>
+                  </AccordionTrigger>
+                  <AccordionContent>
+                    <div className="border-t-[1px] border-gray-200 pt-4 gap-5 flex flex-col">
+                      <div className="flex flex-col gap-1">
+                        <p className="font-medium text-base pb-1">
+                          What We're Looking For
+                        </p>
+                        <p className="font-medium">
+                          • People-Loving & Curious:{" "}
+                          <span className="font-normal">
+                            You enjoy meeting users and learning how they work.
+                          </span>
+                        </p>
+                        <p className="font-medium">
+                          • Clear Communicator:{" "}
+                          <span className="font-normal">
+                            You explain things simply—on calls or in writing.
+                          </span>
+                        </p>
+                        <p className="font-medium">
+                          • Resilient & Driven:{" "}
+                          <span className="font-normal">
+                            You stay motivated, push through challenges, and
+                            take initiative.
+                          </span>
+                        </p>
+                        <p className="font-medium">
+                          • Product & Process Focused:{" "}
+                          <span className="font-normal">
+                            You improve both the product and how we support
+                            users at scale.
+                          </span>
+                        </p>
+                      </div>
+                      <div className="flex flex-col gap-1">
+                        <p className="font-medium text-base pb-1">
+                          About the Role
+                        </p>
+                        <p className="font-medium">
+                          • Support SMB Teams:{" "}
+                          <span className="font-normal">
+                            Help SMBs post-onboarding, answer questions, and
+                            promote relevant features.
+                          </span>
+                        </p>
+                        <p className="font-medium">
+                          • Scale Customer Success:{" "}
+                          <span className="font-normal">
+                            Run scalable programs like Streak Classes to reach
+                            more users efficiently.
+                          </span>
+                        </p>
+                        <p className="font-medium">
+                          • Performance Bonus:{" "}
+                          <span className="font-normal">
+                            Part of your pay is tied to customer success and
+                            engagement.
+                          </span>
+                        </p>
+                      </div>
+                      <ButtonDemo
+                        text="Apply"
+                        className="bg-[#08514D] text-white cursor-pointer"
+                      />
+                    </div>
+                  </AccordionContent>
+                </AccordionItem>
+              </Accordion>
+            </div>
           </div>
         </section>
       </div>
